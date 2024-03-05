3f235162b5bae0b46ef6b0a30a548bfa6d72cd7b
JeinyC <jeiny206@gmail.com>
2024-03-05 20:49:08 +0100

    Astarion init

---
+++ Astarion/src/test/java/com/example/Astarion/AstarionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Astarion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstarionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
